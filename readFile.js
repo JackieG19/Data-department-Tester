440642cfeacec1b2aa67f37f26523d27cffe5b09
Jackie <jacgut19@gmail.com>
2019-02-19 17:42:04 +0000

    still working on a bug

--- readFile.js
+++ readFile.js
@@ -1,35 +1,17 @@
-var departments =["Marketing", "Finance", "Human Resources", "Production", "Development", "Quality Management", "Sales", "Research", "Customer Service"] 
+var departments = ["Marketing", "Finance", "Human Resources", "Production", "Development", "Quality Management", "Sales", "Research", "Customer Service"] 
+var departmentId = ["d001", "d002", "d003", "d004", "d005", "d006", "d007", "d008", "d009"];
+var empolyeeId = [[10017], [], [10005, 10013], [10003, 10004, 10010, 10018, 10020], [10001, 10006, 10008, 10012, 10014, 10018], [10001, 10006, 10008, 10012, 10014, 10018], [10009, 10010], [10002, 10016], [10007, 10015, 10019], [10011]];
+var employeeName = [['Cristinel Bouloucos'], [], ['Kyoichi Maliniak', 'Eberhardt Terkki'], ['Parto Bamford', 'Chirstian Koblick', 'Patricio Bridgland', 'Kazuhide Peha', 'Mayuko Warwick'], ['Georgi Facello', 'Anneke Preusig', 'Saniya Kalloufi', 'Patricio Bridgland', 'Berni Genin', 'Kazuhide Peha'], ['Sumant Peac', 'Duangkaew Piveteau'], ['Bezalel Simmel', 'Kazuhito Cappelletti'], ['Guoxiang Nooteboom', 'Tzvetan Zielinski', 'Lillian Haddadi'], ['Mary Sluis']];
+var salaries = [[99651], [], [94692, 68901], [43311, 74057, 80324, 84672, 47017], [88958, 59755, 0, 54423, 60598, 84672], [94409, 80324], [72527, 77935], [88070, 0, 50032], [0]];
 
-var fs = require('fs');
 
-fs.readFile('department.txt', 'utf8', function(err, data){
-    if(err) throw err;
-    
-    var array = data.split('\n');
-    for(var i = 0; i < array.length; i++){
-        //console.log(array[i]); 
+for(var i = 0; i < departmentId.length; i++){
+    console.log(`Department ${departmentId[i]} - ${departments[i]}`);
+    for(var j = 0; j < empolyeeId[i].length; j++){
+        //console.log(` ${j+1}. Employee ID: ${empolyeeId[i][j]}, Name: ${employeeName[i][j]}`);
+        for(var k = 0; k < salaries[j].length; k++){
+        console.log(` ${j+1}. Employee ID: ${empolyeeId[i][j]}, Name: ${employeeName[i][j]}`, ` ${k+1}. Total Salary: ${salaries[j][k]}`);
+        }
     }
-    
-});
+}
 
-
-
-fs.readFile('employNum.txt', 'utf8', function(err, data){
-    if(err) throw err;
-    
-    var array = data.split('\n');
-    for(var i = 0; i < array.length; i++){
-        //console.log(array[i]); 
-    }
-    
-});
-
-fs.readFile('salaries.txt', 'utf8', function(err, data){
-    if(err) throw err;
-    
-    var array = data.split('\n');
-    for(var i = 0; i < array.length; i++){
-        //console.log(array[i]); 
-    }
-    
-});
